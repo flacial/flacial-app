0d6981c91950d699de59c28d320e0b952860447d
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-05-29 22:13:55 +0400

    feat: Add a marquee to home page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.sass'
 import { GoMarkGithub } from 'react-icons/go';
+import Marquee from "react-fast-marquee";
 
 export default function Home() {
   return (
@@ -11,8 +12,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <nav className={styles.nav} >
-        <a href={'https://github.com/flacial/flacial-app'} target='_blank' rel="noreferrer">You can track the progress on <GoMarkGithub /></a>
+      <nav className={`${styles.nav} ${styles.orangefade}`}>
+        <Marquee pauseOnHover gradientColor={'black'} speed={40}>
+          <a href={'https://github.com/flacial/flacial-app'} target='_blank' rel="noreferrer">You can track the progress on <GoMarkGithub /></a>
+        </Marquee>
       </nav>
       <main className={styles.main}>
         <h1 className={`${styles.title} ${styles.gradient}`}>
