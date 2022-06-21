c3bc0f27034ea3d7977a46431564ecb004890aa1
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-06-21 11:51:19 +0400

    feat: Add Typescript support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import '../styles/globals.css'
 import { usePostHog } from 'next-use-posthog'
-import { POSTHOG_LINK } from '../constants/api.const.ts'
+import { POSTHOG_LINK } from '../constants/api.const'
+import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   if (process.env.NEXT_PUBLIC_POSTHOG_KEY) {
     usePostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
       api_host: POSTHOG_LINK,
