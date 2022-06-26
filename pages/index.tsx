086dc564b939317815b84e89552e86b24b0c0d57
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-06-26 18:40:55 +0400

    feat: Set page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,18 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.sass'
 import { GoMarkGithub } from 'react-icons/go';
+import Layout from '../components/Layout';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Flacial</title>
-        <meta name="description" content="Flacial home and projects" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <nav className={styles.nav} >
-        <a href={'https://github.com/flacial/flacial-app'} target='_blank' rel="noreferrer">You can track the progress on <GoMarkGithub /></a>
-      </nav>
-      <main className={styles.main}>
-        <h1 className={`${styles.title} ${styles.gradient}`}>
-          Coming soon...
-        </h1>
-      </main>
-    </div>
+    <Layout title='Home'>
+        <nav className={styles.nav} >
+          <a href={'https://github.com/flacial/flacial-app'} target='_blank' rel="noreferrer">You can track the progress on <GoMarkGithub /></a>
+        </nav>
+        <main className={styles.main}>
+          <h1 className={`${styles.title} ${styles.gradient}`}>
+            Coming soon...
+          </h1>
+        </main>
+    </Layout>
   )
 }
