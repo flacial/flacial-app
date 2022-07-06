f85a083051df335f0200f98e7206542feb63f650
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-07-06 12:17:08 +0400

    feat: Init Prettier x Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,35 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    "next/core-web-vitals"
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@next/next/recommended',
+    'plugin:prettier/recommended',
+    'prettier'
   ],
-};
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    'react/prop-types': 'off',
+    'react/display-name': 'off'
+  }
+}
